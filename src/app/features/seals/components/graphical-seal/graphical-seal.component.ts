dd085cc0734c77215cc67c5bfbcbd0582acd0208
LAPTOP-6TFFJU85\maria <marian.spoiala@gmail.com>
2021-11-12 07:57:44 +0200

    Added seal drawing routines.

--- src/app/features/seals/components/graphical-seal/graphical-seal.component.ts
+++ src/app/features/seals/components/graphical-seal/graphical-seal.component.ts
@@ -1,5 +1,6 @@
 import { AfterContentInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { Size2D } from './models/size-2d.valueobject';
+
+import { Size2D, Point } from './models';
 
 @Component({
   selector: 'app-graphical-seal',
@@ -30,6 +31,21 @@ export class GraphicalSealComponent implements OnInit, AfterContentInit {
 
     this.drawBackground();
     this.drawBorder();
+    this.drawSealCircle();
+    this.drawSeal([
+      {
+        x: 10, y: 25
+      },
+      {
+        x: 100, y: 250
+      },
+      {
+        x: 200, y: 30
+      },
+      {
+        x: 50, y: 100
+      },
+    ])
   }
 
   private drawBackground(): void {
@@ -38,8 +54,37 @@ export class GraphicalSealComponent implements OnInit, AfterContentInit {
   }
 
   private drawBorder(): void {
-    this.ctx!.strokeStyle = this.foreColor;
+    this.initStroke(this.foreColor, 2)
     this.ctx!.rect(0, 0, this.canvasSize.width, this.canvasSize.height);
+    this.ctx!.stroke();
+  }
+
+  private drawSealCircle(): void {
+    const padding = 4;
+    const radius = Math.min(this.canvasSize.width, this.canvasSize.height) / 2 - padding;
+    this.drawCircle({x: this.canvasSize.width / 2, y: this.canvasSize.height / 2}, radius)
+  }
+
+  private drawSeal(points: Point[]) {
+    this.drawCircle(points[0], 4);
+    this.initStroke(this.foreColor, 2);
+    this.ctx!.moveTo(points[0].x, points[0].y);
+    for(let i = 1; i < points.length; i++) {
+      this.ctx!.lineTo(points[i].x, points[i].y);
+    }
+    this.ctx!.stroke();
+  }
+
+  private initStroke(strokeStyle: string | CanvasGradient | CanvasPattern, lineWidth: number): void {
+    this.ctx!.beginPath();
+    this.ctx!.strokeStyle = strokeStyle;
+    this.ctx!.lineWidth = lineWidth;
+  }
+
+  private drawCircle(center: Point, radius: number): void {
+    this.initStroke(this.foreColor, 2)
+    this.ctx!.arc(center.x, center.y, radius, 0, Math.PI * 2);
+    this.ctx!.stroke();
   }
 
 }
---
+++ src/app/features/seals/components/graphical-seal/models/index.ts
@@ -0,0 +1,2 @@
+export { Size2D } from './size-2d.valueobject';
+export { Point } from './point'
