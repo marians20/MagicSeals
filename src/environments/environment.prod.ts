fd33aa53d79d280a1eea2cfeaf02e8f6eeecbff0
Marian Spoiala <marian.spoiala@gmail.com>
2022-12-11 19:24:43 +0200

    Working on environments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,21 @@
 export const environment = {
   production: true,
+  // firebaseConfig: {
+  //   apiKey: "${{ secrets.API_KEY }}",
+  //   authDomain: "${{ secrets.AUTH_DOMAIN }}",
+  //   projectId: "${{ secrets.PROJECT_ID }}",
+  //   storageBucket: "${{ secrets.STORAGE_BUCKET }}",
+  //   messagingSenderId: "${{ secrets.MESSAGING_SENDER_ID }}",
+  //   appId: "${{ secrets.APP_ID }}",
+  //   measurementId: "${{ secrets.MEASUREMENT_ID }}"
+  // }
   firebaseConfig: {
-    apiKey: "${{ secrets.API_KEY }}",
-    authDomain: "${{ secrets.AUTH_DOMAIN }}",
-    projectId: "${{ secrets.PROJECT_ID }}",
-    storageBucket: "${{ secrets.STORAGE_BUCKET }}",
-    messagingSenderId: "${{ secrets.MESSAGING_SENDER_ID }}",
-    appId: "${{ secrets.APP_ID }}",
-    measurementId: "${{ secrets.MEASUREMENT_ID }}"
+    apiKey: "AIzaSyALUFP1XYXEoZeqV_-_xksdbYHo3FoyPls",
+    authDomain: "magic-sigils.firebaseapp.com",
+    projectId: "magic-sigils",
+    storageBucket: "magic-sigils.appspot.com",
+    messagingSenderId: "221956834710",
+    appId: "1:221956834710:web:65c12fed9dc61f6cf649b7",
+    measurementId: "G-2HC4ZC2KD5"
   }
 };
