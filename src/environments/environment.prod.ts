eec10dda989114ec31b7f36e50d9d9d92b5896d9
Marian Spoiala <marian.spoiala@gmail.com>
2022-12-12 08:04:04 +0200

    Working on environments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,13 +9,4 @@ export const environment = {
     appId: "{APP_ID}",
     measurementId: "{MEASUREMENT_ID}"
   }
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyALUFP1XYXEoZeqV_-_xksdbYHo3FoyPls",
-  //   authDomain: "magic-sigils.firebaseapp.com",
-  //   projectId: "magic-sigils",
-  //   storageBucket: "magic-sigils.appspot.com",
-  //   messagingSenderId: "221956834710",
-  //   appId: "1:221956834710:web:65c12fed9dc61f6cf649b7",
-  //   measurementId: "G-2HC4ZC2KD5"
-  // }
 };
