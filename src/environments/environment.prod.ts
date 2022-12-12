c1140f27ebc27bb5edb079808be57ae1e88c3000
Marian Spoiala <marian.spoiala@gmail.com>
2022-12-12 08:23:16 +0200

    Working on environments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "{API_KEY}",
-    authDomain: "{AUTH_DOMAIN}",
-    projectId: "{PROJECT_ID}",
-    storageBucket: "$STORAGE_BUCKET}",
-    messagingSenderId: "{MESSAGING_SENDER_ID}",
-    appId: "{APP_ID}",
-    measurementId: "{MEASUREMENT_ID}"
+    apiKey: "__API_KEY__",
+    authDomain: "__AUTH_DOMAIN__",
+    projectId: "__PROJECT_ID__",
+    storageBucket: "__$STORAGE_BUCKET__",
+    messagingSenderId: "__MESSAGING_SENDER_ID__",
+    appId: "__APP_ID__",
+    measurementId: "__MEASUREMENT_ID__"
   }
 };
